7f97adac497a87ee8f628f5818a8b3492aa52c03
Ian Whitney <whit0694@umn.edu>
2021-09-25 08:47:15 -0500

    Text sizing/layout fixes for different breakpoints

--- app/components/episode_pagination_component.rb
+++ app/components/episode_pagination_component.rb
@@ -8,13 +8,13 @@ class EpisodePaginationComponent < ViewComponent::Base
     succ = episode.successors((pages_to_display - 1) / 2)
 
     if pred.count > succ.count
-      pred = episode.predecessors(pages_to_display - succ.count)
-    else
-      succ = episode.successors(pages_to_display - pred.count)
+      pred = episode.predecessors(pages_to_display - succ.count - 1)
+    elsif pred.count < succ.count
+      succ = episode.successors(pages_to_display - pred.count - 1)
     end
 
-    @paged_episodes = [succ.reverse, episode, pred].flatten
-    default_classes = %w[text-xs rounded-lg text-center px-10 py-2]
+    @paged_episodes = [succ.reverse, @episode, pred].flatten
+    default_classes = %w[text-xs rounded-lg text-center px-2 md:px-4 lg:px-8 py-2]
     @current_classes = default_classes.dup << "bg-yellow-400"
     @go_to_classes = default_classes.dup << "bg-indigo-400"
   end
